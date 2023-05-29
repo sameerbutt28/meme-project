ce69b97df2d1eaee469c94dd62ead8f9fdcaf909
Sameer Ali Butt <108467884+sameerbutt28@users.noreply.github.com>
2023-05-29 08:38:32 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./components/header"
+import Forms from "./components/forms"
+import NewImg from "./components/buttonimg"
+
+function App() {
+ 
+  return (
+    <div className="App">
+     <Header />
+     <Forms/>
+     <NewImg/>
+    </div>
+  );
+}
+
+export default App;
